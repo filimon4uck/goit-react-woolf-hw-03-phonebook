85e29c5b1c836bafad745f51cd6743bc4456854a
filimon4uck <134231431+filimon4uck@users.noreply.github.com>
2024-02-18 20:15:32 +0100

    added feature 'add to lacalStorage'

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,4 @@
+import styles from 'components/App.module.css';
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <div className={styles.container}>React homework template</div>;
 };
